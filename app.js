d17e13809131e80fc521a659bc36ee3d43e3b9a3
mert <mertaktas231@gmail.com>
2019-11-24 00:01:31 +0300

    filmler sayfası eklendi.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var request = require("request");
 
 app.set("view engine", "ejs");
 
+var img = "https://image.tmdb.org/t/p/w300";
+
 app.get("/", function (req, res) {
     //Sorgulama
     var query = req.query.search;
@@ -14,6 +16,8 @@ app.get("/", function (req, res) {
     // arama url base
     var searchmovie = "https://api.themoviedb.org/3/search/movie?query=";
 
+
+
     // search baglantı tamamı
     var url = searchmovie + query + api;
 
@@ -21,12 +25,35 @@ app.get("/", function (req, res) {
         if (!error && response.statusCode == 200) {
             var source = JSON.parse(body);
             res.render("results", {
-                source: source
+                source: source,
+                img: img
             });
         }
     });
+});
+
+app.get("/movies", function (req, res) {
+    var selectsira = req.query.selectedsira;
+    var siralama = "&sort_by=" + selectsira;
+    //Sorgulama
+    var selectyear = req.query.selectyears;
+    var years = "&year=" + selectyear;
 
 
+    var page = "&page=1"
+
+    var movies = "https://api.themoviedb.org/3/discover/movie?api_key=75019398caf1e5d87c0e4198fc9f17e2&language=en-US&sort_by=popularity.desc";
+
+    var movie = movies + siralama + page + years;
+    request(movie, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movi = JSON.parse(body);
+            res.render("movies", {
+                movi: movi,
+                img: img
+            });
+        }
+    });
 });
 
 var port = process.env.PORT || 8080;
