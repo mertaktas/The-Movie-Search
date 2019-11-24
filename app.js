49e66e9a50ee032cfb5b0e9dbd90adc8574bec71
mert <mertaktas231@gmail.com>
2019-11-25 02:22:15 +0300

    nowplaying_tv sayfası eklendi.

--- app.js
+++ app.js
@@ -196,6 +196,22 @@ app.get("/topratedtv", function (req, res) {
     });
 });
 
+app.get("/nowplayingtv", function (req, res) {
+
+    var people = "https://api.themoviedb.org/3/tv/on_the_air?api_key=75019398caf1e5d87c0e4198fc9f17e2&language=en-US&page=1";
+
+
+    request(people, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var source = JSON.parse(body);
+            res.render("tvs/nowplaying_tv", {
+                source: source,
+                img: img
+            });
+        }
+    });
+});
+
 
 
 
