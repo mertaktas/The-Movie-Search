2f81102f229e4e26c2e0e8287ed25a883b8f05f3
mert <mertaktas231@gmail.com>
2019-11-25 02:12:21 +0300

    populartv sayfası eklendi.

--- app.js
+++ app.js
@@ -164,6 +164,23 @@ app.get("/nowplaying", function (req, res) {
     });
 });
 
+app.get("/populartv", function (req, res) {
+
+    var people = "https://api.themoviedb.org/3/tv/popular?api_key=75019398caf1e5d87c0e4198fc9f17e2&language=en-US&page=1";
+
+
+    request(people, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var source = JSON.parse(body);
+            res.render("tvs/popular_tv", {
+                source: source,
+                img: img
+            });
+        }
+    });
+});
+
+
 
 
 
