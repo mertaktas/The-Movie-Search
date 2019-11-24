3a00658e5fc99d3e03dd18d400637053515933ac
mert <mertaktas231@gmail.com>
2019-11-25 01:33:29 +0300

    populermovie sayfası eklendi.

--- app.js
+++ app.js
@@ -99,6 +99,26 @@ app.get("/people", function (req, res) {
     });
 });
 
+app.get("/populermovie", function (req, res) {
+
+    var people = "https://api.themoviedb.org/3/movie/popular?api_key=75019398caf1e5d87c0e4198fc9f17e2&language=en-US&page=1";
+
+
+    request(people, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var source = JSON.parse(body);
+            res.render("populer_movie", {
+                source: source,
+                img: img
+            });
+        }
+    });
+});
+
+
+
+
+
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Server Has Started!");
