e8f556e3d975e001414dd4636f939c1deeb33830
mert <mertaktas231@gmail.com>
2019-12-09 21:50:24 +0300

    app.js kod düzenlemesi yapıldı.

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-var express = require("express");
-var app = express();
-var request = require("request");
+const express = require("express"),
+    app = express();
 
 app.set("view engine", "ejs");
 
-var indexRoute = require("./router/index");
+// ******* ROUTES *********
+const indexRoute = require("./router/index"),
+    discoverRoute = require("./router/discover"),
+    moviesRoute = require("./router/movies"),
+    tvsRoute = require("./router/tvs"),
+    peopleRoute = require("./router/people");
+
 app.use("/", indexRoute);
-
-var discoverRoute = require("./router/discover");
 app.use("/discover", discoverRoute);
-
-var moviesRoute = require("./router/movies");
 app.use("/movies", moviesRoute);
-
-var tvsRoute = require("./router/tvs");
 app.use("/tvs", tvsRoute);
-
-var peopleRoute = require("./router/people");
 app.use("/people", peopleRoute);
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, function () {
     console.log("Server Has Started!");
 });
