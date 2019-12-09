89baa4e095dadf90e05acb74107fc5f2e16e8f0f
mert <mertaktas231@gmail.com>
2019-12-10 02:34:08 +0300

    bütün keyler data objesine eklendi.

--- data/key.js
+++ data/key.js
@@ -2,7 +2,19 @@ var data = {
     movie_url: "https://api.themoviedb.org/3",
     img: "https://image.tmdb.org/t/p/w300",
     api_key: "api_key=75019398caf1e5d87c0e4198fc9f17e2",
-    search_movie_key: "/search/movie?query="
+    search_movie_key: "/search/movie?query=",
+    sort_by_key: "&sort_by=",
+    year_key: "&year=",
+    discover_movie_key: "/discover/movie",
+    discover_tv_key: "/discover/tv",
+    movie_popular_key: "/movie/popular",
+    movie_toprated_key: "/movie/top_rated",
+    movie_upcoming_key: "/movie/upcoming",
+    movie_nowplaying_key: "/movie/now_playing",
+    tv_popular_key = "/tv/popular",
+    tv_toprated_key = "/tv/top_rated",
+    tv_nowplaying_key = "/tv/on_the_air",
+    person_popular_key: "/person/popular"
 };
 
 module.exports = data;
