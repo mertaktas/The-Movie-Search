4e4eb5734106197fb3164fde54f2975fc50973f6
mert <mertaktas231@gmail.com>
2019-12-10 02:30:01 +0300

    data/key diye dosya eklendi. data objesi oluşturuldu.

---
+++ data/key.js
@@ -0,0 +1,8 @@
+var data = {
+    movie_url: "https://api.themoviedb.org/3",
+    img: "https://image.tmdb.org/t/p/w300",
+    api_key: "api_key=75019398caf1e5d87c0e4198fc9f17e2",
+    search_movie_key: "/search/movie?query="
+};
+
+module.exports = data;
--- router/index.js
+++ router/index.js
@@ -1,16 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var request = require("request");
-
-// the movie url
-var movie_url = "https://api.themoviedb.org/3";
-// img url
-var img = "https://image.tmdb.org/t/p/w300";
-// api key
-var api_key = "api_key=75019398caf1e5d87c0e4198fc9f17e2";
-
-// search movie key
-var search_movie_key = "/search/movie?query=";
+var data = require("../data/key");
 
 router.get("/", function (req, res) {
     res.render('home');
@@ -22,14 +13,14 @@ router.get("/search", function (req, res) {
     var query = req.query.search;
 
     //search baglantı tamamı
-    var url = movie_url + search_movie_key + query + '&' + api_key;
+    var url = data.movie_url + data.search_movie_key + query + '&' + data.api_key;
 
     request(url, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var source = JSON.parse(body);
             res.render("search", {
                 source: source,
-                img: img
+                img: data.img
             });
         }
     });
