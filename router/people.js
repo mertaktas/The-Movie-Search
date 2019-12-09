f65b233364251ad9ca06f485dd0d58a320a33fa2
mert <mertaktas231@gmail.com>
2019-12-10 02:45:05 +0300

    people dosyasına data verileri eklendi.

--- router/people.js
+++ router/people.js
@@ -3,28 +3,19 @@ var router = express.Router();
 var request = require("request");
 var data = require("../data/key");
 
-// the movie url
-var movie_url = "https://api.themoviedb.org/3";
-// img url
-var img = "https://image.tmdb.org/t/p/w300";
-// api key
-var api_key = "api_key=75019398caf1e5d87c0e4198fc9f17e2";
-
-// person popular key
-var person_popular_key = "/person/popular";
-
 router.get("/", function (req, res) {
 
-    var people = movie_url + person_popular_key + '?' + api_key;
+    var people = data.movie_url + data.person_popular_key + '?' + data.api_key;
 
     request(people, function (error, response, body) {
         if (!error && response.statusCode == 200) {
             var source = JSON.parse(body);
             res.render("people", {
                 source: source,
-                img: img
+                img: data.img
             });
         }
     });
 });
+
 module.exports = router;
